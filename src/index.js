9cd70c3b29704f2cb988aad9922fcb0dc16aade5
Elena Ivanova <elenaxivanova@gmail.com>
2020-10-22 21:18:45 +0300

    feat: add check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (!str || !bracketsConfig) {
+    return false
+  }
+  let expression = '';
+  for (i = 0; i < bracketsConfig.length; i++) {
+    const openingBracket = bracketsConfig[i][0];
+    const closingBracket = bracketsConfig[i][1];
+    if ( i > 0 ) {
+      expression += '|';
+    }
+    for (const bracket of [openingBracket, closingBracket]) {
+      if (bracket.match(/[a-zA-Z0-9]/)) {
+        expression += bracket;
+      } else {
+        expression += "\\" + bracket;
+      }
+    }
+  }
+  const regExp = new RegExp(expression, 'g');
+  let stringBefore = str;
+  let stringAfter = '';
+  while (true) {
+    if (stringBefore.length == 0) {
+      return true
+    }
+    stringAfter = stringBefore.replace(regExp, "");
+    if (stringBefore == stringAfter) {
+      return false
+    }
+    stringBefore = stringAfter;
+  }
 }
